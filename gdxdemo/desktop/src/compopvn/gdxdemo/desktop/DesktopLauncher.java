3593d577b5a88dec1bd666425e88961f2ca1d1b9
DESKTOP-GT5HCHD\user <duykhanh94vp@gmail.com>
2018-05-04 22:24:42 +0700

    BEGIN

---
+++ gdxdemo/desktop/src/compopvn/gdxdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package compopvn.gdxdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import compopvn.gdxdemo.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 640;
+		config.height = 480;
+		new LwjglApplication(new GameMain(), config);
+	}
+}
